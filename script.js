597e7e61b66192e309a9fc5653757c118886c89b
CaptainAarav <aaravsahni1037@gmail.com>
2026-03-16 23:26:00 +0000

    Finsihed styling and design now starting js

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('input');
+const convertBtn = document.getElementById('convert-btn');
+const resultsContent = document.getElementById('results-content');
+const resultsOne = document.getElementById('results-one');
+const resultsTwo = document.getElementById('results-two');
+const resultsThree = document.getElementById('results-three');
